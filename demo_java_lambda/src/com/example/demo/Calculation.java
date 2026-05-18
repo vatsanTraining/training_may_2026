4d8ba3c1b0b7d7ca8e45a6f8be1fd672f7c9fa3d
srivatsan <vatsan_k@hotmail.com>
2026-05-18 13:30:45 +0530

    Java Intro Completed

---
+++ demo_java_lambda/src/com/example/demo/Calculation.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+
+
+public interface Calculation<T> {
+
+	 double calculate(T arg) ;
+	
+	
+}
--- demo_java_lambda/src/com/example/demo/EightFuncIfaces.java
+++ demo_java_lambda/src/com/example/demo/EightFuncIfaces.java
@@ -2,11 +2,11 @@ package com.example.demo;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class EightFuncIfaces {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		
 		List<String> names = List.of("Ram","Shyam","Sita","Geetha");
@@ -28,6 +28,15 @@ public class EightFuncIfaces {
 		
 		names.forEach(System.out::println);
 		
+		
+		Calculation<Double> cal = (arg) -> arg *20;
+		
+		System.out.println("Custom Func "+cal.calculate(50.00));
+		
+		
+		Function<Double, Double> cal2 = (arg) -> arg*30;
+		
+		System.out.println("Buit in Func"+cal2.apply(50.00));
 		
 		
 	}
