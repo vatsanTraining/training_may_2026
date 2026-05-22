1258d095861a651a994d46decce0fc35ba237fb9
srivatsan <vatsan_k@hotmail.com>
2026-05-22 10:42:27 +0530

    Boot Admin Server demo added

---
+++ admin_dashboard/src/main/java/com/example/demo/AdminDashboardApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminDashboardApplication.class, args);
+	}
+
+}
+
+
+
+
+   
